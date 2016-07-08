598492ed98897cee8a73dc5ae3d57a217521c5b9
shakuu <todorov.h@gmail.com>
2016-07-08 11:31:27 +0300

    Update: Remove unnecessary files

--- RemoveFoldersExamEdition/RemoveFiles/Contracts/IFileRemover.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RemoveFolders.Utilities.Contracts
-{
-    using System.Collections.Generic;
-
-    public interface IFileRemover
-    {
-        IEnumerable<string> RemoveFilesWithExtension(string path, ICollection<string> extensions);
-    }
-}
